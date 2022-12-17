d3a33cc60a7bac7794b153a4d588ee4ffb903eb1
GreenHatHG <1239776759@qq.com>
2022-12-17 13:12:57 +0800

    feat: beancount query to json file

--- scripts/bean-query.ts
+++ scripts/bean-query.ts
@@ -1,4 +1,5 @@
 import {exec} from 'child_process'
+import * as fs from 'fs'
 import * as CSV from 'csv-string'
 
 function runCommand(command: string, timeout: number): Promise<string>{
@@ -20,11 +21,11 @@ function runCommand(command: string, timeout: number): Promise<string>{
 class QueryResult{
     date: string;
     account: string;
-    position: string; //cost
+    position: number; //cost
     payee: string;
     narration: string; //comment
 
-    constructor(date: string, account: string, position: string, payee: string, narration: string) {
+    constructor(date: string, account: string, position: number, payee: string, narration: string) {
         this.date = date;
         this.account = account;
         this.position = position;
@@ -38,9 +39,32 @@ function buildQueryResult(output: string): Array<QueryResult>{
     parsedCsv.shift()
     return parsedCsv.map(row => {
         row = row.map(str => str.trim())
-        const position = row[2].split(" ")[0]
+        const position = parseFloat(row[2].split(" ")[0])
         return new QueryResult(row[0], row[1], position, row[3], row[4])
     })
+}
+
+function expenseAccountSum(queryResult: Array<QueryResult>): Map<string, string>{
+    let accountSumMap = new Map(); //key: account, value: position sum
+    queryResult.forEach(item => {
+        if(!accountSumMap.has(item.account)){
+            accountSumMap.set(item.account, 0)
+        }
+        accountSumMap.set(item.account, accountSumMap.get(item.account) + item.position)
+    })
+    accountSumMap.forEach((v, k) => accountSumMap.set(k, v.toFixed(2)))
+    return accountSumMap
+}
+
+function toValueNameArrayAndSave(map: Map<string, string>){
+    const keyValueArray = Array.from(map.entries())
+    const objectArray = keyValueArray.map(([k, v]) => ({value: v, name: k}))
+    saveToJsonFile('accountSum', objectArray)
+}
+
+function saveToJsonFile(key: string, value: Object){
+    const json = JSON.stringify({[key]: value})
+    fs.writeFileSync('public/data.json', json,  {encoding: "utf8"})
 }
 
 function buildQueryCommand(): string {
@@ -54,6 +78,7 @@ const timeout = 10 * 1000
 const beanQueryCommand = buildQueryCommand()
 runCommand(beanQueryCommand, timeout)
     .then((result) => {
-        const a = buildQueryResult(result)
-        console.log(a)
+        const queryResult = buildQueryResult(result)
+        let map = expenseAccountSum(queryResult)
+        toValueNameArrayAndSave(map)
     })
